9d8236e9f3b6aab4113d315e4e36d5fd568475c3
KevinHChen <greathongjian@yahoo.com>
2025-04-13 21:19:47 +1200

    Update 1. Two Sum.js

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -3,12 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     hashed = {}
-    for (let i=0; i<nums.length; ++i) {
+    for (let i = 0; i < nums.length; ++i) {
         const num = nums[i]
-        if (hashed.hasOwnProperty(target-num)) {
-            return [i, hashed[target-num]]
+        if (hashed.hasOwnProperty(target - num)) {
+            return [i, hashed[target - num]]
         }
         hashed[num] = i
     }
