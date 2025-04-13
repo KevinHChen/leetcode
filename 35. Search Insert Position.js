4d5da479f801bcc90d1665b7c31752b6aa70f994
KevinHChen <greathongjian@yahoo.com>
2025-04-13 17:59:07 +1200

    Create 35. Search Insert Position.js

---
+++ 35. Search Insert Position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] >= target) {
+            break
+        }
+        i++
+    }
+    return i;
+};
