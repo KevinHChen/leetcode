92b32e659bc5e64056b9a037440a88807e548e48
KevinHChen <greathongjian@yahoo.com>
2025-04-14 20:58:41 +1200

    Create 78. Subsets.js

---
+++ 78. Subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+    let max = 1 << nums.length
+    let result = []
+    for (let i = 0; i < max; ++i) {
+        let temp = []
+        for (let j = 0; j < nums.length; ++j) {
+            if (bitIsOne(i, j)) {
+                temp.push(nums[j])
+            }
+        }
+        result.push(temp)
+    }
+    return result;
+};
+
+var bitIsOne = function (num, bit) {
+    return parseInt(num & (1 << bit)) != 0
+}
