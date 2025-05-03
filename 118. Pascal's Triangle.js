66951e4182462553011903367d858e732bf1c84a
KevinHChen <greathongjian@yahoo.com>
2025-05-03 22:39:02 +1200

    Update 118. Pascal's Triangle.js

--- 118. Pascal's Triangle.js
+++ 118. Pascal's Triangle.js
@@ -1,4 +1,23 @@
 /**
+ * Solution 2: fast, create the whole array rather then pushing new values to an empty array
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    let res = []
+
+    // start with i=0 help to nicely avoid the second loop for the first two rows
+    for (let i = 0; i < numRows; ++i) { 
+        let temp = new Array(i + 1).fill(1) // the first and last elems are always 1
+        for (let j = 1; j < i; ++j) {
+            temp[j] = res[res.length - 1][j - 1] + res[res.length - 1][j]
+        }
+        res.push(temp)
+    }
+    return res
+};
+
+/**
  * Solution 1: Quick, but not fastest
  * @param {number} numRows
  * @return {number[][]}
