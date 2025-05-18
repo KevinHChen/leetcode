49126b9aaf2b0099b02da52395031c0a5736fcfb
KevinHChen <greathongjian@yahoo.com>
2025-05-18 20:49:33 +1200

    Update 77. Combinations.js

--- 77. Combinations.js
+++ 77. Combinations.js
@@ -1,4 +1,31 @@
 /**
+ * Solution 2: >90%, use early prune, also remove the redundant "left" used in solution 1
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+    let results = [];
+    let backtrack = (start, path) => {
+
+        // early prune, if the remaining elems are not suffice to form a total of "k" element, prune it
+        if (path.length + (n - start + 1) < k) return;
+
+        if (path.length == k) {
+            results.push([...path]);
+            return;
+        }
+        for (let i = start; i <= n; ++i) {
+            path.push(i);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+    backtrack(1, [], k);
+    return results;
+};
+
+/**
  * Solution 1: >40%
  * @param {number} n
  * @param {number} k
