2c691ae5eb8f4e8f3de0a6d23cbdac1b4c4db117
KevinHChen <greathongjian@yahoo.com>
2025-05-18 21:10:43 +1200

    Update 74. Search a 2D Matrix.js

--- 74. Search a 2D Matrix.js
+++ 74. Search a 2D Matrix.js
@@ -1,4 +1,28 @@
 /**
+ * Solution 2: simulate the binary search
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    // simulate binary search
+    let left = 0;
+    let right = matrix.length * matrix[0].length - 1;
+    let cols = matrix[0].length;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        // convert the mid value back to [row, col]
+        let row = Math.floor(mid / cols);
+        let col = mid % cols;
+        let curr = matrix[row][col];
+        if (curr == target) return true;
+        else if (curr > target) right = mid - 1;
+        else left = mid + 1;
+    }
+    return false;
+};
+
+/**
  * Solution 1: >8%
  * @param {number[][]} matrix
  * @param {number} target
