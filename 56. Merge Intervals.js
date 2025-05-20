316844670b1805cdf50904cc8f77397d80ddeb6e
KevinHChen <greathongjian@yahoo.com>
2025-05-20 20:26:37 +1200

    Update 56. Merge Intervals.js

--- 56. Merge Intervals.js
+++ 56. Merge Intervals.js
@@ -1,5 +1,33 @@
 /**
- * Solution 1: >6%
+ * Solution 2: >40%
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+    let results = [];
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    let start = intervals[0][0];
+    let end = intervals[0][1];
+    for (let i = 1; i < intervals.length; ++i) {
+        let [newstart, newend] = intervals[i];
+        // new interval
+        if (newstart > end) {
+            results.push([start, end]);
+            start = newstart;
+            end = newend;
+        } else if (newend > end) {
+            end = newend;
+        }
+    }
+    // handle last element
+    results.push([start, end]);
+    return results;
+};
+
+/**
+ * Solution 1: >25%
  * @param {number[][]} intervals
  * @return {number[][]}
  */
